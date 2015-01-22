86caaa4bc0a39d2f4c1a7f09753d9c0f44c4dc86
Austin Herring <awh44@drexel.edu>
2015-01-22 00:07:27 -0500

    Tried to improve the structure of the ClientHandler

--- chessNetwork/ClientHandler.java
+++ chessNetwork/ClientHandler.java
@@ -28,50 +28,69 @@ public class ClientHandler implements Runnable {
 
 	@Override
 	public void run() {
+		int gameID;
 		try {
 			in = new ObjectInputStream(clientSocket.getInputStream());
-			int gameID = in.readInt();
-			if (gameID < 0) {
-				gameID = nextID.getAndIncrement();
-				waitingClients.put(gameID, this);
-				System.out.println("Created gameID = " + gameID + ". Waiting for peer.");
-				waitForPeer();
-				System.out.println("ID = " + gameID + " finished waiting for peer.");
-			}
-			else {
-				ClientHandler other = waitingClients.get(gameID);
-				if (other != null) {
-					out	= new ObjectOutputStream(other.peerArrived(clientSocket.getOutputStream()));
-				}
-				else {
-					return;
-				}
-
-				System.out.println("Game with ID = " + gameID + " joined.");
-			}
-
-			Object obj = in.readObject();
-			while (obj != null) {
-				out.writeObject(obj);
-				out.flush();
-				obj = in.readObject();
-			}
-
-
-			clientSocket.close();
-
+			gameID = in.readInt();
 		}
 		catch (IOException e) {
-			System.out.println("Caught an IOException. Exiting.");
+			readError();
+			return;
 		}
-		catch (InterruptedException e) {
-			System.out.println("Caught an InterruptedException. Exiting.");
+		
+		if (gameID < 0) {
+			try {
+				establishNewGame();
+			}
+			catch (InterruptedException e) {
+				interruptedError();
+				return;
+			}
+		}
+		else {
+			try {
+				joinExistingGame(gameID);
+			}
+			catch (NullPointerException e) {
+				wrongIDError();
+				return;
+			}
+			catch (IOException e) {
+				communicationError();
+				return;
+			}
+		}
+
+		try {
+			readWriteLoop();
+		}
+		catch (IOException e) {
+			communicationError();
+			return;
 		}
 		catch (ClassNotFoundException e) {
-			System.out.println("Caught a ClassNotFoundException. Exiting.");
 		}
 
+		try {
+			clientSocket.close();
+		}
+		catch (IOException e) {
+			socketError();
+			return;	
+		}
+	}
 
+	private void establishNewGame() throws InterruptedException {
+		int gameID = nextID.getAndIncrement();
+		waitingClients.put(gameID, this);
+		System.out.println("Created gameID = " + gameID + ". Waiting for peer.");
+		waitForPeer();
+		System.out.println("ID = " + gameID + " finished waiting for peer.");
+	}
+
+	private void joinExistingGame(int gameID) throws NullPointerException, IOException {
+		out	= new ObjectOutputStream(waitingClients.get(gameID).peerArrived(clientSocket.getOutputStream()));
+		System.out.println("Game with ID = " + gameID + " joined.");
 	}
 
 	private void waitForPeer() throws InterruptedException {
@@ -82,5 +101,34 @@ public class ClientHandler implements Runnable {
 		this.out = new ObjectOutputStream(out);
 		peer.release();
 		return clientSocket.getOutputStream();
+	}
+
+	private void readWriteLoop() throws ClassNotFoundException, IOException {
+		Object obj = in.readObject();
+		while (obj != null) {
+			out.writeObject(obj);
+			out.flush();
+			obj = in.readObject();
+		}
+	}
+
+	private void readError() {
+		System.out.println("Error reading from client. Client thread exiting.");
+	}
+
+	private void interruptedError() {
+		System.out.println("Error in thread (interrupted). Client thread exiting.");
+	}
+
+	private void communicationError() {
+		System.out.println("Error communication between clients. Client thread exiting.");
+	}
+
+	private void socketError() {
+		System.out.println("Error closing socket. Client thread exiting.");
+	}
+
+	private void wrongIDError() {
+		System.out.println("Error in gameID (doesn't exist). Client thread exiting.");
 	}
 }
