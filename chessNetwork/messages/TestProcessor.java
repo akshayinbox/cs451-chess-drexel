747b68166729092c7067cd6f99d752dbdc4748e7
Austin Herring <awh44@drexel.edu>
2015-03-04 12:26:00 -0500

    Removed a test file.

--- chessNetwork/messages/TestProcessor.java
+++
@@ -1,8 +0,0 @@
-package chessNetwork.messages;
-
-public class TestProcessor implements MessageProcessor {
-	public void process(Message message)
-	{
-		System.out.println(message.getContent());
-	}
-}
