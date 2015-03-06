7ed3c393e47106f431108a5c39d8f81786ca4a26
Austin Herring <awh44@drexel.edu>
2015-03-06 15:20:31 -0500

    Added JavaDoc comments to ClientReader/Writer.java

--- chessNetwork/client/ClientReader.java
+++ chessNetwork/client/ClientReader.java
@@ -12,26 +12,27 @@ public class ClientReader implements Runnable, Serializable {
 	private transient ObjectInputStream in;
 	private MessageProcessor processor;
 
-	//imagine passing in a UI object of some kind here and saving it as a member variable
+	/**
+	  * Creates a new ClientReader object which can read from the ObjectInputStream in and which
+	  * passes received Messages off to processor to process
+	  * @param in the ObjectInputStream from which this object will read
+	  * @param processor the MessageProcessor object to which the reader will pass its Messages
+	  */
 	public ClientReader(ObjectInputStream in, MessageProcessor processor) {
 		this.in = in;
 		this.processor = processor;
 	}
 
+	/**
+	  * The run method for the Runnable interface, in which the reader object will enter a loop,
+	  * constantly reading from the server until it is closed or catches some kind exception,
+	  * passing received messages to the MessageProcessor
+	  */
 	@Override
 	public void run() {
 		try {
 			Message received = (Message) in.readObject();
 			while (received != null) {
-				//Here is how I imagine this working with the chess board:
-				//	The UI (or whatever) implements the MessageProcessor interface, and then
-				//	whenever a message is received, the thread passes the received message to the
-				//	UI. Then, the UI queries what type of Message it is: mvoe or chat
-				//	message. If it's a chat message, it prints it to the chat window, and if it's a
-				//	move, applies the move. Right now, this is all happening in this thread, the one that
-				//	receives messages. In the future, if we find that that causes too much blocking,
-				//	we might have to, say, process the message in a short-lived thread, but that is
-				//	down the line.
 				processor.process(received);
 				received = (Message) in.readObject();
 			}
--- chessNetwork/client/ClientWriter.java
+++ chessNetwork/client/ClientWriter.java
@@ -14,19 +14,36 @@ public class ClientWriter implements Runnable, Serializable {
 	private final Semaphore availableMessages = new Semaphore(0);
 	private ObjectOutputStream out;
 
+	/**
+	  * Creates a new ClientWriter object which can write to the specified ObjectOutputStream
+	  * @param out the ObjectOutputStream to which this object will write
+	  */
 	public ClientWriter(ObjectOutputStream out) {
 		this.out = out;
 	}
 
+	/**
+	  * Readies the message to be sent on the interally held queue of messages
+	  * @param m the Message to be sent to the server
+	  */
 	public void send(Message m) {
 		messageQueue.add(m);
 		availableMessages.release();
 	}
 
+	/**
+	  * Tells the writer that writing is done, allowing the run method to exit  after it finishes
+	  * writing all messages in its queue
+	  */
 	public void exit() {
 		availableMessages.release();
 	}
-
+	
+	/**
+	  * The run method for the Runnable interface, in which the writer object will enter a loop,
+	  * writing all Messages passed to its send method to its ObjectOutputStream until it can no
+	  * longer do so, if it is told to exit or receives an exception 
+	  */
 	@Override
 	public void run() {
 		try {
